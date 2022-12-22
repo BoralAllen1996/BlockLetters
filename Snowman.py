74bc8081442270ee436d3e718a8569004bdded70
lenbo <allenbora@gmail.com>
2022-12-23 02:38:32 +0800

    thirteenth

---
+++ Snowman.py
@@ -0,0 +1,24 @@
+# Snowman 
+name = """
+          *     *  
+      *      *       *
+
+      *            *         *
+  *           *           *
+             HHHH
+          HHHHHHHHHH
+          HHHHHHHHHH
+        HHHHHHHHHHHHHHH
+          *  @   @  *
+ K      *      ^      *
+  *       *          *
+   *   SSS  *  SS  * S
+     *  * SSSSSSSSSSSS  *
+      *        0    SS   *   *  * * *K
+    *          0     S     *
+    *          0           *
+     *                    *
+       *               *
+           *   *   *
+"""
+print(name)
